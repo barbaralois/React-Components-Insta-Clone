ed8ef2c8709bc5f53d8bfb9eff7dbdac0747eff8
Barbara Moore <barbaraloismoore@gmail.com>
2020-05-12 18:12:51 -0400

    completed MVP and add comment stretch

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,16 +1,40 @@
 // You will add code to this file
-import React, { useState } from "react";
-import CommentInput from "./CommentInput";
-import Comment from "./Comment";
-import "./Comment.css";
+import React, { useState } from 'react';
+import CommentInput from './CommentInput';
+import Comment from './Comment';
+import './Comment.css';
 
-const CommentSection = props => {
+const CommentSection = (props) => {
   // Add state for the comments
+  const [currentComments, setCurrentComments] = useState(props.comments);
+  const [inputValue, setInputValue] = useState('');
+
+  const newComment = (event) => {
+    event.preventDefault();
+    let comment = {
+      username: 'barbaralois',
+      text: inputValue,
+    };
+    setCurrentComments([...currentComments, comment]);
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {
+        /* map through the comments data and return the Comment component */
+        currentComments.map((singleComment, index) => {
+          return <Comment key={index} comment={singleComment} />;
+        })
+      }
+      <CommentInput
+        submitComment={newComment}
+        comment={inputValue}
+        changeComment={handleChange}
+      />
     </div>
   );
 };
